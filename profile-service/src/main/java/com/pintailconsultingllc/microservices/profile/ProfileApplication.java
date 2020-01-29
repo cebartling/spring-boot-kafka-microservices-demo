f4b6dbd11c709915c8d3a723a199b863f8063cb6
Christopher Bartling <chris.bartling@gmail.com>
2020-01-29 01:49:26 -0600

    Adding profile microservice

---
+++ profile-service/src/main/java/com/pintailconsultingllc/microservices/profile/ProfileApplication.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.microservices.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileApplication.class, args);
+	}
+
+}
---
+++ profile-service/src/test/java/com/pintailconsultingllc/microservices/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.microservices.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
